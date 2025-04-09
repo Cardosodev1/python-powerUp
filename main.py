b79b95245284ab197b114f752481876170a179e5
Guilherme Cardoso <gcs200206@gmail.com>
2025-04-09 13:22:46 -0300

    feat: opening the website for registration

---
+++ main.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+# Set the delay between each action
+pyautogui.PAUSE = 0.5
+
+# opening the website for registration
+pyautogui.press("win")
+pyautogui.write("opera")
+pyautogui.press("enter")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
