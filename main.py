38eba724f7947769d79ccc847ec7cd32fd2e447b
Guilherme Cardoso <gcs200206@gmail.com>
2025-04-09 14:00:56 -0300

    feat: register products

--- main.py
+++ main.py
@@ -26,3 +26,39 @@ time.sleep(1)
 
 # Import the database
 tabela = pandas.read_csv("products.csv")
+
+# Register products
+for linha in tabela.index:
+    pyautogui.click(x = 956, y = 242)
+    code = str(tabela.loc[linha, "codigo"])
+    pyautogui.write(code)
+
+    pyautogui.press("tab")
+    brand = str(tabela.loc[linha, "marca"])
+    pyautogui.write(brand)
+
+    pyautogui.press("tab")
+    type = str(tabela.loc[linha, "tipo"])
+    pyautogui.write(type)
+
+    pyautogui.press("tab")
+    category = str(tabela.loc[linha, "categoria"])
+    pyautogui.write(category)
+    
+    pyautogui.press("tab")
+    price = str(tabela.loc[linha, "preco_unitario"])
+    pyautogui.write(price)
+    
+    pyautogui.press("tab")
+    cost = str(tabela.loc[linha, "custo"])
+    pyautogui.write(cost)
+
+    pyautogui.press("tab")
+    obs = str(tabela.loc[linha, "obs"])
+    if obs != "nan":
+        pyautogui.write(obs)
+
+    pyautogui.press("tab")
+    pyautogui.press("enter")
+
+    pyautogui.scroll(1000)
