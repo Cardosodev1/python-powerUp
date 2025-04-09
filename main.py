070468c0f46e259431d789f983eacce6f1dd2d50
Guilherme Cardoso <gcs200206@gmail.com>
2025-04-09 13:31:20 -0300

    feat: registering on the website

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ import time
 # Set the delay between each action
 pyautogui.PAUSE = 0.5
 
-# opening the website for registration
+# Opening the website for registration
 pyautogui.press("win")
 pyautogui.write("opera")
 pyautogui.press("enter")
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
+
+time.sleep(1)
+
+# Registering on the website
+pyautogui.click(x = 946, y = 361)
+pyautogui.write("guilherme.cardoso@email.com")
+pyautogui.press("tab")
+pyautogui.write("123")
+pyautogui.press("tab")
 pyautogui.press("enter")
 
